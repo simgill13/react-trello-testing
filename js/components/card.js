44563d6fc955a729f8b428357ed329fadea6d58b
Jean-Luc19 <aticus19@gmail.com>
2017-03-05 13:16:29 -0700

    adds board component

---
+++ js/components/card.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Card() {
+  const text = 'This is a Card';
+  return (
+    <div className="card">{text}</div>
+  );
+}
---
+++ js/components/list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './card';
+
+export default function List() {
+  const cards = [];
+  for (let i = 0; i < 3; i++) {
+    cards.push(<Card />);
+  }
+  return (
+    <div className="card-list">
+      {cards}
+    </div>
+  );
+}
